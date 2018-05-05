1307cdfc6c2117bb73da6192191fbecf8e5856b7
Erik Medina <erik.medina.chanca@gmail.com>
2018-05-05 16:03:07 +0200

    Initialize dependency injector in BaseActivity

--- app/src/main/java/com/erikmedina/cityweather/presentation/BaseActivity.kt
+++ app/src/main/java/com/erikmedina/cityweather/presentation/BaseActivity.kt
@@ -1,8 +1,14 @@
 package com.erikmedina.cityweather.presentation
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initializeDependencyInjector()
+    }
 
     protected abstract fun initializeDependencyInjector()
 
